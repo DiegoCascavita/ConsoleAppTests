97e2811adee23d04b3005c60b0914896f3d7c0d4
Diego C <sportscenter857@gmail.com>
2024-08-14 10:48:22 -0500

    [Parse][method]

--- ConsoleAppTests/Program.cs
+++ ConsoleAppTests/Program.cs
@@ -7,12 +7,19 @@
         {
             Console.WriteLine("Hi {0}, I am {1}", to, Name);
         }
+        public static Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+            return person;
+        }
     }
     class Program        
     {
         static void Main(string[] args)
         {
-            var person = new Person();
+            //var person = new Person();
+            var person = Person.Parse("Jhon");
             person.Name = "Jhon";
             person.Introduce("Mclein");
         }
